f8445cc7acf9f75d53334fb0941b7e8569aa2439
2451134 <bds6052@gmail.com>
2023-04-04 21:04:39 +0100

    Fixed CanvasTestPage code using konva

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { memo } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-// import { Stage, Layer, Rect, Text, Circle, Line } from "react-konva"
 
 import Album from "./Album"
 import TopBar from "./TopBar"
@@ -10,38 +9,12 @@ import TopBar from "./TopBar"
 // import NearbyCrimesPage from "../pages/NearbyCrimesPage"
 // import CruisesPage from "../pages/CruisesPage"
 // import BusRoutesPage from "../pages/BusRoutesPage"
-// import CanvasTestPage from "../pages/CanvasTestPage"
+import CanvasTestPage from "../pages/CanvasTestPage"
 
 function App() {
   return (
     <div>
       <TopBar />
-
-      {/* <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Some text on canvas" fontSize={15} />
-          <Rect
-            x={20}
-            y={50}
-            width={100}
-            height={100}
-            fill="red"
-            shadowBlur={10}
-          />
-          <Circle x={200} y={100} radius={50} fill="green" />
-          <Line
-            x={20}
-            y={200}
-            points={[0, 0, 100, 0, 100, 100]}
-            tension={0.5}
-            closed
-            stroke="black"
-            fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-            fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-            fillLinearGradientColorStops={[0, "red", 1, "yellow"]}
-          />
-        </Layer>
-      </Stage> */}
 
       <Router>
         <Routes>
@@ -89,7 +62,7 @@ function BusRoutesPageLink() {
 }
 
 function CanvasTestPageLink() {
-  // return <CanvasTestPage />
+  return <CanvasTestPage />
 }
 
 function NotFound() {
