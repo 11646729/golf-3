d2c0b9c0fe5215f63455c1d0ff98f534d745f52e
2451134 <bds6052@gmail.com>
2023-04-22 10:53:53 +0100

    Moved magic numbers to env file

--- frontend/src/components/CanvasDiagram.jsx
+++ frontend/src/components/CanvasDiagram.jsx
@@ -2,23 +2,23 @@ import React, { useEffect, useState, memo } from "react"
 import { Stage, Layer, Circle, Rect } from "react-konva"
 // import { DrawChartBox } from "./DrawChartBox"
 
-const sizeAdjustment = 0.98
-const shapesOutlineColor = "lightgrey"
-const shapesOutlineWidth = 1
-const topMarginPercentage = 10
-const bottomMarginPercentage = 10
-
 const CanvasDiagram = () => {
   const [rect, setDimensions] = useState({
-    width: window.innerWidth * sizeAdjustment,
-    height: window.innerHeight * sizeAdjustment,
+    width:
+      window.innerWidth * process.env.REACT_APP_GEOPHONEARRAY_SIZEADJUSTMENT,
+    height:
+      window.innerHeight * process.env.REACT_APP_GEOPHONEARRAY_SIZEADJUSTMENT,
   })
 
   useEffect(() => {
     const checkSize = () => {
       setDimensions({
-        width: window.innerWidth * sizeAdjustment,
-        height: window.innerHeight * sizeAdjustment,
+        width:
+          window.innerWidth *
+          process.env.REACT_APP_GEOPHONEARRAY_SIZEADJUSTMENT,
+        height:
+          window.innerHeight *
+          process.env.REACT_APP_GEOPHONEARRAY_SIZEADJUSTMENT,
       })
     }
 
@@ -26,14 +26,16 @@ const CanvasDiagram = () => {
     return () => window.removeEventListener("resize", checkSize)
   }, [])
 
-  console.log(rect)
-
-  const rectangleYPosition = rect.height * (topMarginPercentage / 100)
+  const rectangleYPosition =
+    rect.height *
+    (process.env.REACT_APP_GEOPHONEARRAY_TOPMARGINPERCENTAGE / 100)
   const chartXPosition = rect.width / 2
   const chartYPosition = rect.height / 2
   const circleRadius =
     (rect.height *
-      (1 - topMarginPercentage / 100 - bottomMarginPercentage / 100)) /
+      (1 -
+        process.env.REACT_APP_GEOPHONEARRAY_TOPMARGINPERCENTAGE / 100 -
+        process.env.REACT_APP_GEOPHONEARRAY_BOTTOMMARGINPERCENTAGE / 100)) /
     2
   const rectangleXPosition = (rect.width - circleRadius * 2) / 2
 
@@ -45,15 +47,15 @@ const CanvasDiagram = () => {
           y={rectangleYPosition}
           width={circleRadius * 2}
           height={circleRadius * 2}
-          stroke={shapesOutlineColor}
-          strokeWidth={shapesOutlineWidth}
+          stroke={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINECOLOR}
+          strokeWidth={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINEWIDTH}
         />
         <Circle
           x={chartXPosition}
           y={chartYPosition}
           radius={circleRadius}
-          stroke={shapesOutlineColor}
-          strokeWidth={shapesOutlineWidth}
+          stroke={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINECOLOR}
+          strokeWidth={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINEWIDTH}
         />
       </Layer>
     </Stage>
