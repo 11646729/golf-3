ba37ebc388b99ed1634c470c3a0532d94c3d2a66
2451134 <bds6052@gmail.com>
2023-04-22 13:15:25 +0100

    Refactored CanvasDiagram

--- frontend/src/components/CanvasDiagram.jsx
+++ frontend/src/components/CanvasDiagram.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, memo } from "react"
-import { Stage, Layer, Circle, Rect } from "react-konva"
-// import { DrawChartBox } from "./DrawChartBox"
+import { Stage, Layer } from "react-konva"
+import DrawChartBox from "./DrawChartBox"
 
 const CanvasDiagram = () => {
   const [rect, setDimensions] = useState({
@@ -26,37 +26,10 @@ const CanvasDiagram = () => {
     return () => window.removeEventListener("resize", checkSize)
   }, [])
 
-  const rectangleYPosition =
-    rect.height *
-    (process.env.REACT_APP_GEOPHONEARRAY_TOPMARGINPERCENTAGE / 100)
-  const chartXPosition = rect.width / 2
-  const chartYPosition = rect.height / 2
-  const circleRadius =
-    (rect.height *
-      (1 -
-        process.env.REACT_APP_GEOPHONEARRAY_TOPMARGINPERCENTAGE / 100 -
-        process.env.REACT_APP_GEOPHONEARRAY_BOTTOMMARGINPERCENTAGE / 100)) /
-    2
-  const rectangleXPosition = (rect.width - circleRadius * 2) / 2
-
   return (
     <Stage width={rect.width} height={rect.height} margin={0}>
       <Layer>
-        <Rect
-          x={rectangleXPosition}
-          y={rectangleYPosition}
-          width={circleRadius * 2}
-          height={circleRadius * 2}
-          stroke={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINECOLOR}
-          strokeWidth={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINEWIDTH}
-        />
-        <Circle
-          x={chartXPosition}
-          y={chartYPosition}
-          radius={circleRadius}
-          stroke={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINECOLOR}
-          strokeWidth={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINEWIDTH}
-        />
+        <DrawChartBox rect={rect} />
       </Layer>
     </Stage>
   )
--- frontend/src/components/DrawChartBox.jsx
+++ frontend/src/components/DrawChartBox.jsx
@@ -1,6 +1,46 @@
-import React, { useEffect, useState, memo } from "react"
-import { Rect } from "react-konva"
+import React, { memo } from "react"
+import PropTypes from "prop-types"
+import { Circle, Rect } from "react-konva"
 
-const DrawChartBox = () => {}
+const DrawChartBox = (props) => {
+  const { rect } = props
+
+  DrawChartBox.propTypes = {
+    rect: PropTypes.array,
+  }
+
+  const rectangleYPosition =
+    rect.height *
+    (process.env.REACT_APP_GEOPHONEARRAY_TOPMARGINPERCENTAGE / 100)
+  const chartXPosition = rect.width / 2
+  const chartYPosition = rect.height / 2
+  const circleRadius =
+    (rect.height *
+      (1 -
+        process.env.REACT_APP_GEOPHONEARRAY_TOPMARGINPERCENTAGE / 100 -
+        process.env.REACT_APP_GEOPHONEARRAY_BOTTOMMARGINPERCENTAGE / 100)) /
+    2
+  const rectangleXPosition = (rect.width - circleRadius * 2) / 2
+
+  return (
+    <div>
+      <Rect
+        x={rectangleXPosition}
+        y={rectangleYPosition}
+        width={circleRadius * 2}
+        height={circleRadius * 2}
+        stroke={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINECOLOR}
+        strokeWidth={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINEWIDTH}
+      />
+      <Circle
+        x={chartXPosition}
+        y={chartYPosition}
+        radius={circleRadius}
+        stroke={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINECOLOR}
+        strokeWidth={process.env.REACT_APP_GEOPHONEARRAY_SHAPESOUTLINEWIDTH}
+      />
+    </div>
+  )
+}
 
 export default memo(DrawChartBox)
