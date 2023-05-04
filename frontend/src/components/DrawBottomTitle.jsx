7c362d86dcc895d550e657d89cd582b30bd3c259
2451134 <bds6052@gmail.com>
2023-05-04 10:06:49 +0100

    Added Top & Bottom Title text code

--- frontend/src/components/DrawBottomTitle.jsx
+++ frontend/src/components/DrawBottomTitle.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react"
 import PropTypes from "prop-types"
-import { Rect } from "react-konva"
+import { Rect, Text } from "react-konva"
 
 const DrawBottomTitle = (props) => {
   const { rect } = props
@@ -12,18 +12,32 @@ const DrawBottomTitle = (props) => {
   }
 
   return (
-    <Rect
-      x={rect.left}
-      y={rect.top}
-      width={rect.right - rect.left}
-      height={rect.bottom - rect.top}
-      stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
-      strokeWidth={parseInt(
-        process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH,
-        10
-      )}
-      fill="orange"
-    />
+    <>
+      <Rect
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
+        strokeWidth={parseInt(
+          process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH,
+          10
+        )}
+        fill="orange"
+      />
+      <Text
+        fontSize={16}
+        text={process.env.REACT_APP_GEOPHONEARRAY_TOPTITLETEXT}
+        stroke="grey"
+        strokeWidth={1}
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        align="center"
+        verticalAlign="middle"
+      />
+    </>
   )
 }
 
--- frontend/src/components/DrawTopTitle.jsx
+++ frontend/src/components/DrawTopTitle.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react"
 import PropTypes from "prop-types"
-import { Rect } from "react-konva"
+import { Rect, Text } from "react-konva"
 
 const DrawTopTitle = (props) => {
   const { rect } = props
@@ -11,21 +11,33 @@ const DrawTopTitle = (props) => {
     rect: PropTypes.object,
   }
 
-  // console.log(rect)
-
   return (
-    <Rect
-      x={rect.left}
-      y={rect.top}
-      width={rect.right - rect.left}
-      height={rect.bottom - rect.top}
-      stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
-      strokeWidth={parseInt(
-        process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH,
-        10
-      )}
-      fill="lightyellow"
-    />
+    <>
+      <Rect
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
+        strokeWidth={parseInt(
+          process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH,
+          10
+        )}
+        fill="lightyellow"
+      />
+      <Text
+        fontSize={16}
+        text={process.env.REACT_APP_GEOPHONEARRAY_TOPTITLETEXT}
+        stroke="grey"
+        strokeWidth={1}
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        align="center"
+        verticalAlign="middle"
+      />
+    </>
   )
 }
 
