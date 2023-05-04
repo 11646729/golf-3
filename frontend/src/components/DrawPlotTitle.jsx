0ce4e4027cdbeddf9fedb372a2be62f932019591
2451134 <bds6052@gmail.com>
2023-05-04 09:59:32 +0100

    Added PlotTitle text code

--- frontend/src/components/DrawPlotTitle.jsx
+++ frontend/src/components/DrawPlotTitle.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react"
 import PropTypes from "prop-types"
-import { Rect } from "react-konva"
+import { Rect, Text } from "react-konva"
 
 const DrawPlotTitle = (props) => {
   const { rect } = props
@@ -12,17 +12,31 @@ const DrawPlotTitle = (props) => {
   }
 
   return (
-    <Rect
-      x={rect.left}
-      y={rect.top}
-      width={rect.right - rect.left}
-      height={rect.bottom - rect.top}
-      stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
-      strokeWidth={parseInt(
-        process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH
-      )}
-      fill="lightgreen"
-    />
+    <>
+      <Rect
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
+        strokeWidth={parseInt(
+          process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH
+        )}
+        fill="lightgreen"
+      />
+      <Text
+        fontSize={18}
+        text={process.env.REACT_APP_GEOPHONEARRAY_PLOTTITLETEXT}
+        stroke="black"
+        strokeWidth={1}
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        align="center"
+        verticalAlign="middle"
+      />
+    </>
   )
 }
 
