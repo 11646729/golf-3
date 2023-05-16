79776336584c3a3a70e912175c6f49de610b0550
2451134 <bds6052@gmail.com>
2023-05-16 08:31:31 +0100

    Added Legend Title & Dimension Label

--- frontend/src/components/DrawLegendArea.jsx
+++ frontend/src/components/DrawLegendArea.jsx
@@ -1,10 +1,12 @@
 import React, { memo } from "react"
 import PropTypes from "prop-types"
-import { Rect } from "react-konva"
+import { Rect, Text } from "react-konva"
 
 const DrawLegendArea = (props) => {
   const { rect } = props
 
+  // If DrawLegend !== true then return
+  if (process.env.REACT_APP_GEOPHONEARRAY_DRAWLEGEND !== "true") return
   // If rect is null then do not draw the Rectangle
   if (!rect) return
   // If rect width = 0 then do not draw the Rectangle
@@ -15,18 +17,47 @@ const DrawLegendArea = (props) => {
   }
 
   return (
-    <Rect
-      x={rect.left}
-      y={rect.top}
-      width={rect.right - rect.left}
-      height={rect.bottom - rect.top}
-      stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
-      strokeWidth={parseInt(
-        process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH,
-        10
-      )}
-      // fill="lightcyan"
-    />
+    <>
+      {/* Outline Rectangle for Legend */}
+      <Rect
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
+        strokeWidth={parseInt(
+          process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH,
+          10
+        )}
+        // fill="lightcyan"
+      />
+      {/* Legend Title */}
+      <Text
+        fontSize={16}
+        text={process.env.REACT_APP_GEOPHONEARRAY_LEGENDTEXT}
+        stroke="grey"
+        strokeWidth={0.5}
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        align="center"
+        verticalAlign="top"
+      />
+      {/* Legend Dimension Label */}
+      <Text
+        fontSize={16}
+        text={process.env.REACT_APP_GEOPHONEARRAY_LEGENDVALUESTEXT}
+        stroke="grey"
+        strokeWidth={0.5}
+        x={rect.left}
+        y={rect.top}
+        width={rect.right - rect.left}
+        height={rect.bottom - rect.top}
+        align="left"
+        verticalAlign="middle"
+      />
+    </>
   )
 }
 
