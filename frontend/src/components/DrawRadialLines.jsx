e50b59bd823eb6b77cf8b271e492ccbae5fd88fd
2451134 <bds6052@gmail.com>
2023-05-20 22:37:34 +0100

    Added radial lines code

--- frontend/src/components/DrawRadialLines.jsx
+++ frontend/src/components/DrawRadialLines.jsx
@@ -11,20 +11,45 @@ const DrawRadialLines = (props) => {
     rect: PropTypes.object,
   }
 
-  const y1 = rect.top
-  const x1 = rect.left + (rect.right - rect.left) / 2
-  const y2 = rect.top + (rect.bottom - rect.top) / 2
-  const x2 = rect.left + (rect.right - rect.left) / 2
-
-  return (
-    <Line
-      points={[x1, y1, x2, y2]}
-      stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
-      strokeWidth={parseInt(
-        process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH
-      )}
-    />
+  const NoOfAngles = parseInt(
+    process.env.REACT_APP_GEOPHONEARRAY_M3DRADIALNOOFANGLES
   )
+
+  const pi_rad = Math.PI / 180
+
+  if (
+    process.env.REACT_APP_GEOPHONEARRAY_M3DRADIALDISPLAYRADIALLINES === "true"
+  ) {
+    const lines = []
+
+    for (let i = 1; i <= NoOfAngles; i++) {
+      const dOffx =
+        ((rect.bottom - rect.top) / 2) *
+        Math.sin((360 / NoOfAngles) * pi_rad * i)
+      const dOffy =
+        0 -
+        ((rect.bottom - rect.top) / 2) *
+          Math.cos((360 / NoOfAngles) * pi_rad * i)
+
+      const y1 = rect.top + (rect.bottom - rect.top) / 2
+      const x1 = rect.left + (rect.right - rect.left) / 2
+      const y2 = rect.top + (rect.bottom - rect.top) / 2 + dOffy
+      const x2 = rect.left + (rect.right - rect.left) / 2 + dOffx
+
+      lines.push(
+        <Line
+          key={i}
+          points={[x1, y1, x2, y2]}
+          stroke={process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINECOLOR}
+          strokeWidth={parseInt(
+            process.env.REACT_APP_GEOPHONEARRAY_CHARTOUTLINEWIDTH
+          )}
+        />
+      )
+    }
+
+    return <>{lines}</>
+  }
 }
 
 export default memo(DrawRadialLines)
