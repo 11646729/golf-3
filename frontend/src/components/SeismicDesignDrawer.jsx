bd918373bf25fe08b154c0c2469ec880546be9dd
2451134 <bds6052@gmail.com>
2023-06-22 07:18:19 +0100

    Fixed Drawer error in SeismicDesignDrawer

--- frontend/src/components/SeismicDesignDrawer.jsx
+++ frontend/src/components/SeismicDesignDrawer.jsx
@@ -1,99 +1,56 @@
-import React, { memo } from "react"
-import Box from "@mui/material/Box"
 import Drawer from "@mui/material/Drawer"
 import Button from "@mui/material/Button"
-import List from "@mui/material/List"
 import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemText from "@mui/material/ListItemText"
-import InboxIcon from "@mui/icons-material/MoveToInbox"
-import MailIcon from "@mui/icons-material/Mail"
-import Link from "@mui/material/Link"
+import { ListItemIcon, ListItemText, ListItemButton, Link } from "@mui/material"
+import {
+  CheckBoxOutlineBlankOutlined,
+  InboxOutlined,
+  MailOutline,
+} from "@mui/icons-material"
 
-const SeismicDesignDrawer = () => {
-  const [state, setState] = React.useState({
-    left: false,
-  })
+import { useState, memo } from "react"
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    console.log(anchor)
+const data = [
+  {
+    name: "Array Pattern Design",
+    icon: <InboxOutlined />,
+    link: "/seismicarraydesignpage",
+  },
+  {
+    name: "2D Array Response Plot",
+    icon: <CheckBoxOutlineBlankOutlined />,
+    link: "/seismicarraydesignpage",
+  },
+  {
+    name: "3D Array Response Plot",
+    icon: <MailOutline />,
+    link: "/3darrayresponseplotpage",
+  },
+]
 
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return
-    }
+const TestDrawer = () => {
+  const [open, setOpen] = useState(false)
 
-    setState({ ...state, [anchor]: open })
-  }
-
-  const list = (anchor) => (
-    <Box
-      sx={{ display: "flex" }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      {/* <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
-
-      <List>
-        <ListItem key={"Array Pattern Design"} disablePadding>
-          <ListItemButton component={Link} to="/seismicarraydesignpage">
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary={"Array Pattern Design"} />
+  const getList = () => (
+    <div style={{ width: 250 }} onClick={() => setOpen(false)}>
+      {data.map((item, index) => (
+        <ListItem key={index}>
+          <ListItemButton component={Link} to={item.link}>
+            {/* <ListItemIcon>{item.icon}</ListItemIcon> */}
+            <ListItemText primary={item.name} />
           </ListItemButton>
         </ListItem>
-        <ListItem key={"2D Array Response Plot"} disablePadding>
-          <ListItemButton component={Link} to="/seismicarraydesignpage">
-            <ListItemIcon>
-              <MailIcon />
-            </ListItemIcon>
-            <ListItemText primary={"2D Array Response Plot"} />
-          </ListItemButton>
-        </ListItem>
-        <ListItem key={"3D Array Response Plot"} disablePadding>
-          <ListItemButton component={Link} to="/3darrayresponseplotpage">
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary={"3D Array Response Plot"} />
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Box>
+      ))}
+    </div>
   )
-
   return (
     <div>
-      {["Change Screens"].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
+      <Button onClick={() => setOpen(true)}>Click me</Button>
+      <Drawer open={open} anchor={"left"} onClose={() => setOpen(false)}>
+        {getList()}
+      </Drawer>
     </div>
   )
 }
 
-export default memo(SeismicDesignDrawer)
+export default memo(TestDrawer)
