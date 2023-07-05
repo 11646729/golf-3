d5140caeb934505e38b2910113798a9df25945db
2451134 <bds6052@gmail.com>
2023-07-05 08:18:38 +0100

    Update Seismic Designs

--- backend/controllers/seismicDesignsController.js
+++ backend/controllers/seismicDesignsController.js
@@ -124,6 +124,88 @@ const deleteSeismicDesigns = (db) => {
 }
 
 // -------------------------------------------------------
+// Import Seismic Designs Table in the SQLite Database
+// Path: Function called in switchBoard
+// -------------------------------------------------------
+export const importSeismicDesignsData = (req, res) => {
+  // Open a Database Connection
+  let db = null
+  db = openSqlDbConnection(process.env.SQL_URI)
+
+  // Guard clause for null Database Connection
+  if (db === null) return
+
+  try {
+    // Fetch all the Seismic Designs data
+    fs.readFile(
+      // process.env.RAW_GOLF_COURSE_DATA_FILEPATH,
+      "utf8",
+      (err, data) => {
+        if (err) {
+          console.error(err.message)
+        }
+
+        // Save the data in the seismicdesigns Table in the SQLite database
+        const courses = JSON.parse(data)
+        populateSeismicDesigns(courses)
+      }
+    )
+  } catch (err) {
+    console.error("Error in importSeismicDesignsData: ", err.message)
+  }
+
+  // Close the Database Connection
+  closeSqlDbConnection(db)
+}
+
+// -------------------------------------------------------
+// Local function
+// -------------------------------------------------------
+const populateSeismicDesigns = (courses) => {
+  // Open a Database Connection
+  let db = null
+  db = openSqlDbConnection(process.env.SQL_URI)
+
+  let loop = 0
+  try {
+    do {
+      // courseid == loop
+      const course = [
+        loop,
+        // process.env.DATABASE_VERSION,
+        // process.env.TYPE_GOLF_CLUB,
+        // courses.crs.properties.name,
+        // courses.features[loop].properties.name,
+        // courses.features[loop].properties.phoneNumber,
+        // courses.features[loop].properties.photoTitle,
+        // courses.features[loop].properties.photoUrl,
+        // courses.features[loop].properties.description,
+        // courses.features[loop].geometry.coordinates[0],
+        // courses.features[loop].geometry.coordinates[1],
+      ]
+
+      const sql =
+        "INSERT INTO seismicdesigns (seismicdesignsid, databaseversion) VALUES ($1, $2, $3 )"
+
+      db.run(sql, course, (err) => {
+        if (err) {
+          console.error(err.message)
+        }
+      })
+
+      loop++
+    } while (loop < courses.features.length)
+
+    console.log("No of new Seismic Designs created & saved: ", loop)
+  } catch (e) {
+    console.error(e.message)
+  }
+
+  // Close the Database Connection
+  closeSqlDbConnection(db)
+}
+
+// -------------------------------------------------------
 // Get all Seismic Designs from SQLite database
 // Path: localhost:4000/api/golf/getSeismicDesigns
 // -------------------------------------------------------
