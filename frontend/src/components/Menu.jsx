5a919f29d62bf3f7a669a604906db8625d4adf3a
2451134 <bds6052@gmail.com>
2023-07-22 07:13:29 +0100

    Changed color of svgs

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -7,33 +7,45 @@ const Menu = () => {
       <div className="item">
         <span className="title">Main</span>
         <Link to="/rawdatapage">
-          <img src="" alt="" />
+          <img className="listItemIcon" src="" alt="" />
           <span className="listItemTitle">Raw Data</span>
         </Link>
         <Link to="/">
-          <img src="" alt="" />
+          <img className="listItemIcon" src="" alt="" />
           <span className="listItemTitle">Profile</span>
         </Link>
       </div>
       <div className="item">
         <span className="title">Weather</span>
         <Link to="/weatherpage">
-          <img src="" alt="" />
+          <img className="listItemIcon" src="" alt="" />
           <span className="listItemTitle">Weather</span>
         </Link>
       </div>
       <div className="item">
         <span className="title">Seismic Array Design</span>
         <Link to="/seismicarraydesignpage">
-          <img src="/static/images/scatter_plot_black_24dp.svg" alt="" />
+          <img
+            className="listItemIcon"
+            src="/static/images/scatter_plot_black_24dp.svg"
+            alt=""
+          />
           <span className="listItemTitle">Array Design</span>
         </Link>
         <Link to="/seismicarraydesignpage">
-          <img src="/static/images/ssid_chart_black_24dp.svg" alt="" />
+          <img
+            className="listItemIcon"
+            src="/static/images/ssid_chart_black_24dp.svg"
+            alt=""
+          />
           <span className="listItemTitle">2D Radial Design</span>
         </Link>
         <Link to="/3darrayresponseplotpage">
-          <img src="/static/images/pie_chart_outline_black_24dp.svg" alt="" />
+          <img
+            className="listItemIcon"
+            src="/static/images/pie_chart_outline_black_24dp.svg"
+            alt=""
+          />
           <span className="listItemTitle">3D Radial Design</span>
         </Link>
       </div>
