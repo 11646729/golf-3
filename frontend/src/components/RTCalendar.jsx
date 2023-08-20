36a6f9e26c6fbf79db128f1511bc0114cc7b2189
2451134 <bds6052@gmail.com>
2023-08-20 09:41:06 +0100

    Format RTCalendar

--- frontend/src/components/RTCalendar.jsx
+++ frontend/src/components/RTCalendar.jsx
@@ -1,32 +1,53 @@
 import React, { useState, useEffect, memo } from "react"
 import { getRTCalendarData } from "../functionHandlers/loadRTCalendarDataHandler"
+import moment from "moment"
 import "../styles/calendar.scss"
+import "../data"
+import { calendarEvents } from "../data"
 
 // -------------------------------------------------------
 // React Controller component
 // -------------------------------------------------------
 const RTCalendar = () => {
-  const [calendarEvents, setCalendarEventsData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  // const [calendarEvents, setCalendarEventsData] = useState([])
+  // const [isLoading, setIsLoading] = useState(true)
 
-  const calendarDataUrl = "http://localhost:4000/api/golf/getGolfCourses"
+  // const calendarDataUrl = "http://localhost:4000/api/golf/getGolfCourses"
 
-  useEffect(() => {
-    getRTCalendarData(calendarDataUrl)
-      .then((returnedData) => {
-        setCalendarEventsData(returnedData)
+  // useEffect(() => {
+  //   getRTCalendarData(calendarDataUrl)
+  //     .then((returnedData) => {
+  //       // setCalendarEventsData(returnedData)
+  //       // setIsLoading(false)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+  // }, [])
 
-        setIsLoading(false)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+  // console.log(calendarEvents)
+  // var aistime = calendarEvents.tableData[0].DTSTAMP
+
+  // console.log(aistime)
+  console.log(moment.utc(calendarEvents.tableData[0].DTSTAMP).format("hh:mm"))
 
   return (
-    <div className="calendarmain">
-      <div className="calendartitle">Calendar Events for 17/08/23</div>
-      <div className="calendareventscontainer">Time Description</div>
+    <div className="table">
+      <div className="caption">Calendar Events for Today Mon 17/08/23</div>
+      <div className="thead">
+        {/* <div className="tr"> */}
+        {/* <div className="th">Time</div>
+        <div className="th">Description</div> */}
+        {/* </div> */}
+      </div>
+      {calendarEvents.tableData.map((item) => (
+        <div className="tbody" key={item.id}>
+          <div className="eventtime">
+            {moment.utc(item.DTSTAMP).format("hh:mm")}
+          </div>
+          <div className="eventdescription">{item.event}</div>
+        </div>
+      ))}
     </div>
   )
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,17 @@
+export const calendarEvents = {
+  title: "Total Visit",
+  // color: "#FF8042",
+  // dataKey: "visit",
+  tableData: [
+    {
+      id: 1,
+      DTSTAMP: "20191018T091330Z",
+      event: "Fly to Dubai",
+    },
+    {
+      id: 2,
+      DTSTAMP: "20191018T121330Z",
+      event: "Book Tee Time",
+    },
+  ],
+}
