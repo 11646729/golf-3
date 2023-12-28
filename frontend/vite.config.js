0fbf869a65d5278db52753be7cd16a85f5e8635f
2451134 <bds6052@gmail.com>
2023-12-28 09:55:52 +0000

    Debug vite files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,8 @@
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: "http://localhost:5173/",
+  },
 })
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
