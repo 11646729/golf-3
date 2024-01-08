8b5126d70af9d403ff62bb70287f2a06d7e74970
2451134 <bds6052@gmail.com>
2024-01-08 09:09:56 +0000

    Uncommented mockCalenderDetails code

--- rtMockDatafeed/generateMockCalendarData.js
+++ rtMockDatafeed/generateMockCalendarData.js
@@ -32,29 +32,29 @@ export const generateMockCalendarData = (indexCount) => {
     }
 
     let latestReading2 = {
-      //   kind: "calendar#event",
-      //   etag: '"3405692353396000"',
-      //   id: "_84ok4chm611j4b9h6h0kab9k70r48ba26h2jiba26ssj0hhp88r3ccq18g",
-      //   status: "confirmed",
-      //   htmlLink:
-      //     "https://www.google.com/calendar/event?eid=Xzg0b2s0Y2htNjExajRiOWg2aDBrYWI5azcwcjQ4YmEyNmgyamliYTI2c3NqMGhocDg4cjNjY3ExOGcgYmRzNjA1MkBt&ctz=Europe/London",
-      //   created: "2023-11-27T19:17:59.000Z",
-      //   updated: "2023-12-17T20:49:36.698Z",
-      //   summary: "Xmas Carol Service",
-      //   location:
-      //     "St. Mark's Church\nChurch St, Newtownards, BT23 4AN, Northern Ireland",
-      //   creator: { email: "bds6052@gmail.com", self: true },
-      //   organizer: { email: "bds6052@gmail.com", self: true },
-      //   start: { dateTime: "2023-12-17T19:00:00Z", timeZone: "Europe/London" },
-      //   end: { dateTime: "2023-12-17T20:00:00Z", timeZone: "Europe/London" },
-      //   iCalUID: "A1B260C2-14AE-486D-B4E9-B790F9B663AD",
-      //   sequence: 2,
-      //   reminders: { useDefault: true },
-      //   eventType: "default",
+      kind: "calendar#event",
+      etag: '"3405692353396000"',
+      id: "_84ok4chm611j4b9h6h0kab9k70r48ba26h2jiba26ssj0hhp88r3ccq18g",
+      status: "confirmed",
+      htmlLink:
+        "https://www.google.com/calendar/event?eid=Xzg0b2s0Y2htNjExajRiOWg2aDBrYWI5azcwcjQ4YmEyNmgyamliYTI2c3NqMGhocDg4cjNjY3ExOGcgYmRzNjA1MkBt&ctz=Europe/London",
+      created: "2023-11-27T19:17:59.000Z",
+      updated: "2023-12-17T20:49:36.698Z",
+      summary: "Xmas Carol Service",
+      location:
+        "St. Mark's Church\nChurch St, Newtownards, BT23 4AN, Northern Ireland",
+      creator: creatorOrganizerDetails,
+      organizer: creatorOrganizerDetails,
+      start: startEndDateDetails,
+      end: startEndDateDetails,
+      iCalUID: "A1B260C2-14AE-486D-B4E9-B790F9B663AD",
+      sequence: 2,
+      reminders: remindersDetails,
+      eventType: "default",
     }
 
     mockCalendarMessageBody.push(latestReading1)
-    // mockCalendarMessageBody.push(latestReading2)
+    mockCalendarMessageBody.push(latestReading2)
 
     return mockCalendarMessageBody
   } catch (error) {
