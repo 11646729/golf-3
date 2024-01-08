051381279e521081e53a777520072e130d8e95d3
2451134 <bds6052@gmail.com>
2024-01-08 08:24:17 +0000

    Remove obsolete file

--- backend/listenForRabbitMQMessages.js
+++ backend/listenForRabbitMQMessages.js
@@ -1,47 +1,160 @@
-import { connect } from "amqplib"
+// Management Console
+// http://localhost:15672
+
+import amqp from "amqplib/callback_api.js"
 import { rabbitMQ } from "./rtNewsMSconfig.js"
+import { emitTemperatureData } from "./controllers/rtWeatherController.js"
+import { emitNewsHeadlinesData } from "./controllers/rtNewsController.js"
 
-//  -------------------------------------
-//  THIS IS WORKING CODE - DO NOT DELETE
-//  -------------------------------------
+export const listenForRabbitMQMessages = (io) => {
+  amqp.connect(rabbitMQ.exchangeUrl, (error, connection) => {
+    if (error) {
+      console.log("Fatal Error - Cannot connect to exchange")
+      throw error
+    }
 
-export const listenForRabbitMQMessages = async () => {
-  const connection = await connect(rabbitMQ.exchangeUrl)
-  const channel = await connection.createChannel()
+    // var amqpConnection = amqp.createConnection(amqpConnectionDetails);
+    // amqpConnection.on('ready', function(){
+    //   var exchange = amqpConnection.exchange('some_exchange', {'passive':true});
+    //   exchange.on('error', function(error) {
+    //     done();
+    //    });
+    //    exchange.on('open', function(exchange) {
+    //      throw("exchange exists!")
+    //      done();
+    //    });
+    //  });
 
-  await channel.assertExchange(rabbitMQ.exchangeName, rabbitMQ.exchangeType, {
-    durable: false,
-  })
+    connection.createChannel((error1, channel) => {
+      if (error1) {
+        console.log("Fatal Error - Cannot create a channel to exchange")
+        throw error1
+      }
 
-  // const qc = await channel.assertQueue("calendarQueue")
-  // await channel.bindQueue(qc.queue, rabbitMQ.exchangeName, "calendar")
-  // console.log(qc.queue + " queue created")
+      channel.assertExchange(rabbitMQ.exchangeName, rabbitMQ.exchangeType, {
+        durable: false,
+      })
 
-  // const qn = await channel.assertQueue("newsQueue")
-  // await channel.bindQueue(qn.queue, rabbitMQ.exchangeName, "news")
-  // console.log(qn.queue + " queue created")
+      channel.assertQueue(
+        "calendarQueue",
+        {
+          durable: false,
+        },
+        (error2, qc) => {
+          if (error2) {
+            console.log("Fatal Error - Cannot create a calendarQueue")
+            throw error2
+          }
+          console.log("calendarQueue created & bound to exchange")
 
-  const qw = await channel.assertQueue("weatherQueue", {
-    durable: false,
-  })
-  await channel.bindQueue(qw.queue, rabbitMQ.exchangeName, "weather")
-  console.log(qw.queue + " queue created")
+          channel.bindQueue(qc.queue, rabbitMQ.exchangeName, "calendar")
+        }
+      )
 
-  // channel.consume(qc.queue, (msg) => {
-  //   const data = JSON.parse(msg.content)
-  //   console.log(data)
-  //   channel.ack(msg)
-  // })
+      channel.assertQueue(
+        "newsQueue",
+        {
+          durable: false,
+        },
+        (error2, qn) => {
+          if (error2) {
+            console.log("Fatal Error - Cannot create a newsQueue")
+            throw error2
+          }
+          console.log("newsQueue created & bound to exchange")
 
-  // channel.consume(qn.queue, (msg) => {
-  //   const data = JSON.parse(msg.content)
-  //   console.log(data)
-  //   channel.ack(msg)
-  // })
+          channel.bindQueue(qn.queue, rabbitMQ.exchangeName, "news")
+        }
+      )
 
-  channel.consume(qw.queue, (msg) => {
-    const data = JSON.parse(msg.content)
-    console.log(data)
-    channel.ack(msg)
+      channel.assertQueue(
+        "weatherQueue",
+        {
+          durable: false,
+        },
+        (error2, qw) => {
+          if (error2) {
+            console.log("Fatal Error - Cannot create a weatherQueue")
+            throw error2
+          }
+          console.log("weatherQueue created & bound to exchange")
+
+          channel.bindQueue(qw.queue, rabbitMQ.exchangeName, "weather")
+        }
+      )
+
+      io.on("connection", (socket) => {
+        console.log("New RabbitMQ client connected")
+        console.log(socket.id)
+
+        channel.consume(
+          "calendarQueue",
+          (payload) => {
+            if (payload != null) {
+              let contents = JSON.parse(payload.content)
+              console.log("===== Receive =====")
+              console.log(contents)
+            }
+          },
+          {
+            noAck: true,
+          }
+        )
+
+        channel.consume(
+          "newsQueue",
+          (payload) => {
+            if (payload != null) {
+              let contents = JSON.parse(payload.content)
+
+              // Emit 2 events isLoading & sendData to client
+              try {
+                if (contents != null) {
+                  // Emit 2 events isLoading & sendData to client
+                  emitNewsHeadlinesData(socket, contents.message, false)
+                }
+              } catch (error) {
+                console.log("Error in listenForRabbitMQMessages1: ", error)
+              }
+
+              // console.log("===== Receive =====")
+              // console.log(contents)
+            }
+          },
+          {
+            noAck: true,
+          }
+        )
+
+        channel.consume(
+          "weatherQueue",
+          (payload) => {
+            if (payload != null) {
+              let contents = JSON.parse(payload.content)
+
+              // Emit 2 events isLoading & sendData to client
+              try {
+                if (contents != null) {
+                  emitTemperatureData(socket, contents.message, false)
+                }
+              } catch (error) {
+                console.log("Error in listenForRabbitMQMessages1: ", error)
+              }
+
+              // console.log("===== Receive =====")
+              // console.log(contents)
+            }
+            // })
+          },
+          {
+            noAck: true,
+          }
+        )
+
+        // socket.on("disconnect", () => {
+        //   console.log("Client disconnected")
+        // })
+      })
+    })
   })
 }
