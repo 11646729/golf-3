02316839b63b9a04a5f2dde28b51f8241b8fa50d
2451134 <bds6052@gmail.com>
2024-02-20 10:47:39 +0000

    Minor refactoring

--- mockdatafrontend/src/components/index.jsx
+++ mockdatafrontend/src/components/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client"
-import App from "./components/App.jsx"
+import App from "./App.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
