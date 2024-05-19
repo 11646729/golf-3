5dac269bdbbb9436a604923fceaa258a4df1f332
2451134 <bds6052@gmail.com>
2024-05-19 09:48:44 +0100

    Removed unrequired code

--- frontend/src/components/CruisesTable2.jsx
+++ frontend/src/components/CruisesTable2.jsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useState, memo } from "react"
 import PropTypes from "prop-types"
 import Paper from "@mui/material/Paper"
 import Table from "@mui/material/Table"
@@ -36,29 +36,6 @@ const columns = [
   },
 ]
 
-function createData(name, code, population, size) {
-  const density = population / size
-  return { name, code, population, size, density }
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
-]
-
 const CruisesTable = (props) => {
   const { portArrivals } = props
 
@@ -66,8 +43,8 @@ const CruisesTable = (props) => {
     portArrivals: PropTypes.array,
   }
 
-  const [page, setPage] = React.useState(0)
-  const [rowsPerPage, setRowsPerPage] = React.useState(10)
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage)
@@ -162,4 +139,4 @@ const CruisesTable = (props) => {
   )
 }
 
-export default CruisesTable
+export default memo(CruisesTable)
