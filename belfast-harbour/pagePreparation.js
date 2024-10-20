b7d1ae50ca0acebc51eb2288c709e923d7bf4943
11646729 <bds6052@gmail.com>
2024-10-20 09:40:16 +0100

    Added UserAgent to pagePreparation

--- belfast-harbour/pagePreparation.js
+++ belfast-harbour/pagePreparation.js
@@ -4,6 +4,10 @@ export const pagePreparationObject = {
     // Load initial Web page
     let browser = await browserInstance
     let page = await browser.newPage()
+    await page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0.1 Safari/605.1.15"
+    )
+
     const url = urlString
 
     // Navigate to the selected page & wait for it to download
