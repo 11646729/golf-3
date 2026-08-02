cfeb9498d0192057592c9fd0a6f034cc2d031472
11646729 <bds6052@gmail.com>
2026-08-02 09:31:54 +0100

    Changes to Comments

--- backend/routes/cruiseRouteCatalog.js
+++ backend/routes/cruiseRouteCatalog.js
@@ -15,28 +15,42 @@ var cruiseRouter = express.Router()
 // ---------------------------------------------------
 // Belfast Harbour Cruise Schedule Routes
 // ---------------------------------------------------
+// ---------------------------------------------------
 // GET catalogue home page
+// ---------------------------------------------------
 cruiseRouter.get("/", index)
 
+// ---------------------------------------------------
 // POST trigger import of Belfast Harbour cruise schedule PDF
+// ---------------------------------------------------
 cruiseRouter.post("/importBelfastSchedule", importBelfastSchedule)
 
+// ---------------------------------------------------
 // GET Belfast Harbour cruise schedule data
+// ---------------------------------------------------
 cruiseRouter.get("/getBelfastSchedule", getBelfastSchedule)
 
+// ---------------------------------------------------
 // GET current Belfast import job status (for frontend polling)
+// ---------------------------------------------------
 cruiseRouter.get("/getBelfastImportStatus", (_req, res) =>
   res.json(getBelfastImportStatus()),
 )
 
+// ---------------------------------------------------
 // GET current AIS vessel positions
+// ---------------------------------------------------
 cruiseRouter.get("/vesselPositions", getVesselPositions)
 
-// GET / POST AIS geographic filter state
+// ---------------------------------------------------
+// GET / POST AIS geographic filter state: Called from CruiseMap
+// ---------------------------------------------------
 cruiseRouter.get("/geoFilter", getAisGeoFilter)
 cruiseRouter.post("/geoFilter", setAisGeoFilter)
 
+// ---------------------------------------------------
 // POST scrape current vessel position from CruiseMapper  { vesselname }
+// ---------------------------------------------------
 cruiseRouter.post("/scrapePosition", scrapeVesselPosition)
 
 export default cruiseRouter
